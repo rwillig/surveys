63760e8d4e71cc7c66301b007518a47f068a934b
Micha Niskin <micha.niskin@gmail.com>
2014-01-06 18:11:58 -0500

    login

--- src/clj/surveys/api.clj
+++ src/clj/surveys/api.clj
@@ -1,8 +1,18 @@
 (ns surveys.api
   (:refer-clojure :exclude [defn])
-  (:require [surveys.rules :refer [allow deny]]
+  (:require [surveys.rules :as rules]
             [tailrecursion.castra :refer [defn]]))
 
 (defn doit []
-  {:rpc [(allow)]}
+  {:rpc [(rules/allow)]}
   "This came from the server.")
+
+(defn get-state [& [email]]
+  {:rpc [(or (not email) (rules/self email))]}
+  (if-not email
+    nil
+    {:hi :world}))
+
+(defn login [email password]
+  {:rpc [(rules/login email password)]}
+  (get-state email))
--- src/clj/surveys/rules.clj
+++ src/clj/surveys/rules.clj
@@ -4,3 +4,9 @@
 
 (def allow (constantly true))
 (def deny  #(throw (ex auth "Not Allowed.")))
+(def self  #(or (= (:email @*session*) %1) (deny)))
+
+(defn login [email pass]
+  (if (and (= email "foop") (= pass "foop"))
+    (swap! *session* assoc :email email)
+    (throw (ex auth "Wrong email or password."))))
